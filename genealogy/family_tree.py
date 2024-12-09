AndreiToroplean/genealogy

commit 678bb2a9de06e096318315e51ed437cf536ec3cb

Revise the nomenclature throughout the project to avoid using abbreviations.

genealogy/family_tree.py
@@ -4,7 +4,7 @@ from math import inf
 import random
 
 from genealogy.person import Person
-from genealogy.utils import Rel
+from genealogy.utils import Relationship
 
 
 class FamilyTree:
@@ -12,20 +12,20 @@ class FamilyTree:
     def from_json(cls, json_data: str) -> "FamilyTree":
         data = json.loads(json_data)
         people_dict = {}
-        
+
         # Create all Person objects
         for id_, name in data["people"].items():
             person = Person(id_, name)
             people_dict[id_] = person
-            
+
         # Set up relationships
         for child_id, parents in data["relationships"].items():
             child = people_dict[child_id]
-            for rel, parent_id in parents.items():
+            for relationship, parent_id in parents.items():
                 parent = people_dict[parent_id]
-                child.parents[Rel[rel]] = parent
+                child.parents[Relationship[relationship]] = parent
                 parent.children.append(child)
-                
+
         return cls(people_dict.values())
 
     def __init__(self, people: Iterable[Person]):
@@ -42,7 +42,7 @@ class FamilyTree:
             },
             "relationships": {
                 person.id: {
-                    rel.name: parent.id 
+                    rel.name: parent.id
                     for rel, parent in person.parents.items()
                 }
                 for person in self.people
@@ -66,9 +66,9 @@ class FamilyTree:
             else:
                 break
 
-            def append(node):
-                sorted_nodes.append(node)
-            node.p_dfs(visited, append)
+            def append(n):
+                sorted_nodes.append(n)
+            node.traverse_children_depth_first(visited, append)
             append(node)
 
         sorted_nodes.reverse()
@@ -82,8 +82,8 @@ class FamilyTree:
         for i, person in enumerate(self.people):
             if random.random() < p_skip:
                 continue
-            for j, pot_child in zip(range(len(self.people[:i]) - 1, -1, -1), reversed(self.people[:i])):
-                if pot_child in person.children:
+            for j, potential_child in zip(range(len(self.people[:i]) - 1, -1, -1), reversed(self.people[:i])):
+                if potential_child in person.children:
                     break
             else:
                 if skip_if_not_found:
@@ -97,8 +97,8 @@ class FamilyTree:
         for i, person in enumerate(self.people):
             if random.random() < p_skip:
                 continue
-            for j, pot_parent in enumerate(self.people[i + 1:], start=i + 1):
-                if pot_parent in person.parents.values():
+            for j, potential_parent in enumerate(self.people[i + 1:], start=i + 1):
+                if potential_parent in person.parents.values():
                     break
             else:
                 if skip_if_not_found:
@@ -111,17 +111,17 @@ class FamilyTree:
     def _compute_generations(self):
         for i, person in enumerate(self.people):
             for child in person.children:
-                for pot_child in self.people[:i]:
-                    if pot_child == child:
-                        person.generation = max(person.generation, pot_child.generation + 1)
+                for potential_child in self.people[:i]:
+                    if potential_child == child:
+                        person.generation = max(person.generation, potential_child.generation + 1)
                 else:
                     continue
 
         for i, person in zip(range(len(self.people) - 1, -1, -1), reversed(self.people)):
-            min_gen = inf
+            min_generation = inf
             for parent in person.parents.values():
-                for pot_parent in self.people[i:]:
-                    if pot_parent == parent:
-                        min_gen = min(min_gen, pot_parent.generation)
-            if min_gen != inf:
-                person.generation = min_gen - 1
+                for potential_parent in self.people[i:]:
+                    if potential_parent == parent:
+                        min_generation = min(min_generation, potential_parent.generation)
+            if min_generation != inf:
+                person.generation = min_generation - 1

genealogy/person.py
@@ -1,18 +1,18 @@
-from .utils import Rel
+from .utils import Relationship
 
 
 class Person:
     NEE = " ne.e "  # Class constant for maiden name separator
 
-    def __init__(self, id, name="", parents=None, children=None, generation=0):
-        self.id = id
+    def __init__(self, id_, name="", parents=None, children=None, generation=0):
+        self.id = id_
         self.first_name = ""
         self.last_name = ""
         self.middle_name = ""
         self.maiden_name = ""
         if name:
             self.name = name
-        self.parents: dict[Rel, Person] = parents if parents is not None else {}
+        self.parents: dict[Relationship, Person] = parents if parents is not None else {}
         self.children: list[Person] = children if children is not None else []
         self.generation = generation
 
@@ -58,22 +58,22 @@ class Person:
             < (other.last_name, other.maiden_name, other.first_name, other.middle_name)
         )
 
-    def p_dfs(self, visited, func):
+    def traverse_children_depth_first(self, visited, func):
         if self in visited: return False
         visited.append(self)
 
         for parent in self.parents.values():
-            r = parent.p_dfs(visited, func)
+            r = parent.traverse_children_depth_first(visited, func)
             if not r: continue
             func(parent)
         return True
 
-    def c_dfs(self, visited, func):
+    def traverse_parents_depth_first(self, visited, func):
         if self in visited: return False
         visited.append(self)
 
         for child in self.children:
-            r = child.c_dfs(visited, func)
+            r = child.traverse_parents_depth_first(visited, func)
             if not r: continue
             func(child)
             return True

genealogy/family_tree_renderer.py
@@ -1,8 +1,8 @@
 from itertools import zip_longest
 
 from genealogy.family_tree import FamilyTree
-from genealogy.surface import Surface, ArrowsSurface, SurfPos
-from genealogy.utils import ARRS
+from genealogy.surface import Surface, ArrowsSurface, SurfacePosition
+from genealogy.utils import ARROWS
 
 
 class FamilyTreeRenderer:
@@ -14,131 +14,132 @@ class FamilyTreeRenderer:
         self.family_tree = family_tree
 
         self._coords = None
-        self._cxs = None
-        self._names_surf = None
-        self._arrows_surf = None
-        self._surf = None
+        self._connections = None
+        self._names_surface = None
+        self._arrows_surface = None
+        self._surface = None
 
     def render(self) -> str:
         self._draw_surf()
-        return self._surf.as_str
+        return self._surface.as_str
 
     def _draw_surf(self):
         self._coords = {}
-        self._cxs = []
-        self._names_surf = Surface()
-        self._arrows_surf = ArrowsSurface()
-        self._surf = Surface()
+        self._connections = []
+        self._names_surface = Surface()
+        self._arrows_surface = ArrowsSurface()
+        self._surface = Surface()
 
-        self._draw_names_surf()
-        self._draw_arrows_surf()
-        self._compress_surfs_vertically()
-        self._surf = self._names_surf + self._arrows_surf
-        self._surf.add_line()
+        self._draw_names_surface()
+        self._draw_arrows_surface()
+        self._compress_surfaces_vertically()
+        self._surface = self._names_surface + self._arrows_surface
+        self._surface.add_line()
 
-    def _draw_names_surf(self):
-        prev_gen = -1
+    def _draw_names_surface(self):
+        prev_generation = -1
         line = 0
         for person in self.family_tree.people:
-            line += 1 if person.generation > prev_gen else 2
-            prev_gen = person.generation
-            self._names_surf.draw(SurfPos.from_gen(line, person.generation), person.name)
-            self._coords[person.id] = SurfPos.from_gen(line, person.generation)
+            line += 1 if person.generation > prev_generation else 2
+            prev_generation = person.generation
+            self._names_surface.draw(SurfacePosition.from_generation(line, person.generation), person.name)
+            self._coords[person.id] = SurfacePosition.from_generation(line, person.generation)
 
-    def _draw_arrows_surf(self):
-        cxs = self._generate_cxs()
-        cxs = self._allocate_channels(cxs)
-        self._arrows_surf.draw_connections(cxs)
+    def _draw_arrows_surface(self):
+        connections = self._generate_connections()
+        connections = self._allocate_channels(connections)
+        self._arrows_surface.draw_connections(connections)
 
     @staticmethod
-    def _allocate_channels(cxs):
-        for gen, gen_cxs in enumerate(cxs):
-            gen_cxs = list(gen_cxs.values())
-            for cx in gen_cxs:
-                cx_lines = list(zip(*cx[0], *cx[1]))[0]
-                cx.extend([min(cx_lines), max(cx_lines), None])
+    def _allocate_channels(connections):
+        for generation, generation_connections in enumerate(connections):
+            generation_connections = list(generation_connections.values())
+            for connection in generation_connections:
+                connection_lines = list(zip(*connection[0], *connection[1]))[0]
+                connection.extend([min(connection_lines), max(connection_lines), None])
 
-            chs_usages = [[] for _ in gen_cxs]
-            for cx in gen_cxs:
-                _, _, cx_min, cx_max, channel = cx
+            channel_usages = [[] for _ in generation_connections]
+            for connection in generation_connections:
+                _, _, connection_min, connection_max, channel = connection
                 if channel is not None:
                     continue
 
-                for pot_ch, ch_usages in enumerate(chs_usages):
-                    for usage in ch_usages:
-                        us_min, us_max = usage
-                        if not (cx_max < us_min or us_max < cx_min):
+                for potential_channel, usage_ranges in enumerate(channel_usages):
+                    for usage_range in usage_ranges:
+                        usage_min, usage_max = usage_range
+                        if not (connection_max < usage_min or usage_max < connection_min):
                             break
 
                     else:
-                        cx[4] = pot_ch
-                        ch_usages.append((cx_min, cx_max))
+                        connection[4] = potential_channel
+                        usage_ranges.append((connection_min, connection_max))
                         break
-        return cxs
+        return connections
 
-    def _generate_cxs(self):
-        cxs = [{} for _ in set(person.generation for person in self.family_tree.people)]
+    def _generate_connections(self):
+        connections = [{} for _ in set(person.generation for person in self.family_tree.people)]
         for person in self.family_tree.people:
             if not person.parents.values():
                 continue
 
-            c_coords = self._coords[person.id] + [1, 0]
-            p_coords = [self._coords[parent.id] for parent in person.parents.values()]
+            child_coords = self._coords[person.id] + [1, 0]
+            parent_coords = [self._coords[parent.id] for parent in person.parents.values()]
 
-            gen_cxs = cxs[person.generation]
+            generation_connections = connections[person.generation]
             couple_id = tuple(sorted([parent.id for parent in person.parents.values()]))
             try:
-                cx = gen_cxs[couple_id]
+                connection = generation_connections[couple_id]
             except KeyError:
-                gen_cxs[couple_id] = [p_coords, [c_coords]]
+                generation_connections[couple_id] = [parent_coords, [child_coords]]
             else:
-                cx[1].append(c_coords)
+                connection[1].append(child_coords)
 
-        return cxs
+        return connections
 
-    def _compress_surfs_vertically(self):
+    def _compress_surfaces_vertically(self):
         i = 0
         names_ended = False
         arrows_ended = False
         while True:
             try:
-                names_line = self._names_surf[i]
+                names_line = self._names_surface[i]
             except IndexError:
                 names_line = []
                 names_ended = True
             try:
-                arrows_line = self._arrows_surf[i]
+                arrows_line = self._arrows_surface[i]
             except IndexError:
                 arrows_line = []
                 arrows_ended = True
             if names_ended and arrows_ended:
                 break
 
-            if not set(arrows_line).issubset([ARRS["middle"], None]):
+            if not set(arrows_line).issubset([ARROWS["middle"], None]):
                 i += 1
                 continue
 
             if not names_line:
-                del self._names_surf[i]
-                del self._arrows_surf[i]
+                del self._names_surface[i]
+                del self._arrows_surface[i]
                 continue
 
             if i == 0:
                 i += 1
                 continue
 
-            prev_arrows_line = self._arrows_surf[i - 1]
-            prev_names_line = self._names_surf[i - 1]
+            prev_arrows_line = self._arrows_surface[i - 1]
+            prev_names_line = self._names_surface[i - 1]
 
-            for nam_c, prev_nam_c, prev_arr_c in zip_longest(names_line, prev_names_line, prev_arrows_line, fillvalue=None):
-                p, q, r = nam_c is not None, prev_nam_c is not None, prev_arr_c is not None
+            for name_arrow, prev_name_arrow, prev_arrow_arrow in zip_longest(
+                    names_line, prev_names_line, prev_arrows_line, fillvalue=None):
+                p, q, r = name_arrow is not None, prev_name_arrow is not None, prev_arrow_arrow is not None
                 if p and q or p and r or q and p:
                     break
 
             else:
-                del self._arrows_surf[i]
-                self._names_surf[i - 1] = self._names_surf[i - 1] + names_line
-                del self._names_surf[i]
+                del self._arrows_surface[i]
+                self._names_surface[i - 1] = self._names_surface[i - 1] + names_line
+                del self._names_surface[i]
                 continue
 
             i += 1

genealogy/surface.py
@@ -1,6 +1,6 @@
 from itertools import zip_longest
 
-from genealogy.utils import ARRS, ARRS_ARITHMETIC
+from genealogy.utils import ARROWS, ARROWS_ARITHMETIC
 
 
 class Surface(list):
@@ -9,19 +9,19 @@ class Surface(list):
             raise DrawError("line must be positive. ")
 
         self._extend_to_line(pos.line)
-        has_collided = self[pos.line].draw(pos.index, iterable, up_to=up_to, no_overwrite=no_overwrite)
+        has_collided = self[pos.line].draw(pos.index_, iterable, up_to=up_to, no_overwrite=no_overwrite)
         return has_collided
 
     def add_line(self):
-        self.append(SurfLine())
+        self.append(SurfaceLine())
 
     def _extend_to_line(self, line):
         if len(self) <= line:
-            self.extend([SurfLine() for _ in range(line - len(self) + 1)])
+            self.extend([SurfaceLine() for _ in range(line - len(self) + 1)])
 
     def __add__(self, other):
         rtn = Surface()
-        for a_line, b_line in zip_longest(self, other, fillvalue=SurfLine()):
+        for a_line, b_line in zip_longest(self, other, fillvalue=SurfaceLine()):
             rtn.append(a_line + b_line)
         return rtn
 
@@ -30,7 +30,7 @@ class Surface(list):
         return "\n".join([line.as_str for line in self])
 
 
-class SurfLine(list):
+class SurfaceLine(list):
     @classmethod
     def drawn(cls, index, iterable):
         self = cls()
@@ -42,7 +42,7 @@ class SurfLine(list):
             index -= len(iterable)
 
         if index < 0:
-            raise DrawError("index must be positive. ")
+            raise DrawError("index_ must be positive. ")
 
         if len(self) < index:
             self.extend([None for _ in range(index - len(self))])
@@ -51,96 +51,104 @@ class SurfLine(list):
             return False
 
         has_collided = False
-        for i, char in enumerate(iterable):
+        for i, arrow in enumerate(iterable):
             if len(self) == index + i:
-                self.append(char)
+                self.append(arrow)
                 continue
 
-            prev_char = self[index + i]
-            if prev_char is not None:
+            prev_arrow = self[index + i]
+            if prev_arrow is not None:
                 has_collided = True
-                if no_overwrite and char == ARRS["co"]:
+                if no_overwrite and arrow == ARROWS["connection"]:
                     continue
-            self[index + i] = char
+            self[index + i] = arrow
         return has_collided
 
     def __add__(self, other):
-        rtn = SurfLine()
-        for char, other_char in zip_longest(self, other, fillvalue=None):
-            if char is not None:
-                rtn.append(char)
+        rtn = SurfaceLine()
+        for arrow, other_arrow in zip_longest(self, other, fillvalue=None):
+            if arrow is not None:
+                rtn.append(arrow)
                 continue
 
-            rtn.append(other_char)
+            rtn.append(other_arrow)
         return rtn
 
     @property
     def as_str(self):
-        return "".join([char if char is not None else " " for char in self])
+        return "".join([arrow if arrow is not None else " " for arrow in self])
 
 
 class ArrowsSurface(Surface):
-    def draw_connections(self, cxs):
-        for gen, gen_cxs in enumerate(cxs):
-            for cx in gen_cxs.values():
-                p_coords, c_coords, cx_min, cx_max, channel = cx
-                self._draw_channel(gen, channel, cx_min, cx_max)
-                for c_coord in c_coords:
-                    self._draw_child_co(c_coord, channel)
-                for p_coord in p_coords:
-                    self._draw_parent_co(p_coord, gen, channel)
+    def draw_connections(self, connections):
+        for generation, generation_connections in enumerate(connections):
+            for connection in generation_connections.values():
+                parent_coords, child_coords, connection_min, connection_max, channel = connection
+                self._draw_channel(generation, channel, connection_min, connection_max)
+                for child_coord in child_coords:
+                    self._draw_child_connection(child_coord, channel)
+                for parent_coord in parent_coords:
+                    self._draw_parent_connection(parent_coord, generation, channel)
 
-    def _draw_channel(self, gen, channel, start, end):
+    def _draw_channel(self, generation, channel, start, end):
         for line in range(start, end + 1):
-            pos = SurfPos.from_gen(line, gen).co_right(channel)
+            pos = SurfacePosition.from_generation(line, generation).connection_right(channel)
             if line == start:
                 if line == end:
-                    self.draw(pos, ARRS["co"])
+                    self.draw(pos, ARROWS["connection"])
                     continue
-                self.draw(pos, ARRS["start"])
+                self.draw(pos, ARROWS["start"])
             elif line == end:
-                self.draw(pos, ARRS["end"])
+                self.draw(pos, ARROWS["end"])
             else:
-                self.draw(pos, ARRS["middle"])
+                self.draw(pos, ARROWS["middle"])
 
-    def _draw_child_co(self, pos, channel):
-        co_start_pos = pos.co_tail
-        co_end_pos = pos.co_right(channel)
-        co_len = co_end_pos.index - co_start_pos.index - len(ARRS["tail"])
-        arr = ARRS["tail"] + ARRS["co"] * co_len + self._co_char(co_end_pos, ARRS["left"])
-        self.draw(co_start_pos, arr, no_overwrite=True)
+    def _draw_child_connection(self, pos, channel):
+        connection_start_pos = pos.connection_tail
+        connection_end_pos = pos.connection_right(channel)
+        connection_len = connection_end_pos.index_ - connection_start_pos.index_ - len(ARROWS["tail"])
+        arrow = (
+            ARROWS["tail"]
+            + ARROWS["connection"] * connection_len
+            + self._get_connection_arrow(connection_end_pos, ARROWS["left"])
+        )
+        self.draw(connection_start_pos, arrow, no_overwrite=True)
 
-    def _draw_parent_co(self, p_pos, c_gen, channel):
-        co_start_pos = p_pos.co_left(channel, c_gen)
-        co_end_pos = p_pos.co_head
-        co_len = co_end_pos.index - co_start_pos.index - len(ARRS["head"])
-        arr = self._co_char(co_start_pos, ARRS["right"]) + ARRS["co"] * (co_len - 1) + ARRS["head"]
-        self.draw(co_start_pos, arr, no_overwrite=True)
+    def _draw_parent_connection(self, parent_pos, child_generation, channel):
+        connection_start_pos = parent_pos.connection_left(channel, child_generation)
+        connection_end_pos = parent_pos.connection_head
+        connection_len = connection_end_pos.index_ - connection_start_pos.index_ - len(ARROWS["head"])
+        arrow = (
+            self._get_connection_arrow(connection_start_pos, ARROWS["right"])
+            + ARROWS["connection"] * (connection_len - 1)
+            + ARROWS["head"]
+        )
+        self.draw(connection_start_pos, arrow, no_overwrite=True)
 
-    def _co_char(self, pos, co_char):
+    def _get_connection_arrow(self, pos, connection_arrow):
         try:
-            prev_char = self[pos.line][pos.index]
+            prev_arrow = self[pos.line][pos.index_]
         except IndexError:
-            prev_char = None
+            prev_arrow = None
 
-        if prev_char is None:
-            return co_char
+        if prev_arrow is None:
+            return connection_arrow
 
-        return ARRS_ARITHMETIC[(prev_char, co_char)]
+        return ARROWS_ARITHMETIC[(prev_arrow, connection_arrow)]
 
 
-class SurfPos(list):
-    _gen_shift = 16
+class SurfacePosition(list):
+    _generation_shift = 16
     _start_shift = 4
-    _first_channel_shift = _start_shift + len(ARRS["tail"])
-    _add_channel_shift = 2
+    _first_channel_shift = _start_shift + len(ARROWS["tail"])
+    _additional_channel_shift = 2
 
     @classmethod
-    def from_gen(cls, line, gen):
-        return SurfPos([line, gen * cls._gen_shift])
+    def from_generation(cls, line, generation):
+        return SurfacePosition([line, generation * cls._generation_shift])
 
     def __add__(self, other):
-        return SurfPos([p_dim + p_dim_other for p_dim, p_dim_other in zip(self, other)])
+        return SurfacePosition([p_dim + p_dim_other for p_dim, p_dim_other in zip(self, other)])
 
     def __iadd__(self, other):
         for dim in range(len(self)):
@@ -148,25 +156,30 @@ class SurfPos(list):
         return self
 
     def __sub__(self, other):
-        return SurfPos([p_dim - p_dim_other for p_dim, p_dim_other in zip(self, other)])
+        return SurfacePosition([p_dim - p_dim_other for p_dim, p_dim_other in zip(self, other)])
 
     def __isub__(self, other):
         for dim in range(len(self)):
             self[dim] -= other[dim]
         return self
 
-    def co_right(self, channel):
-        return self + [0, self._first_channel_shift + channel * self._add_channel_shift]
+    def connection_right(self, channel):
+        return self + [0, self._first_channel_shift + channel * self._additional_channel_shift]
 
-    def co_left(self, channel, c_gen):
-        return SurfPos([self.line, self._first_channel_shift + channel * self._add_channel_shift + self._gen_shift * c_gen])
+    def connection_left(self, channel, child_generation):
+        index = (
+            self._first_channel_shift
+            + channel * self._additional_channel_shift
+            + self._generation_shift * child_generation
+        )
+        return SurfacePosition([self.line, index])
 
     @property
-    def co_tail(self):
+    def connection_tail(self):
         return self + [0, self._start_shift]
 
     @property
-    def co_head(self):
+    def connection_head(self):
         return self
 
     @property
@@ -174,7 +187,7 @@ class SurfPos(list):
         return self[0]
 
     @property
-    def index(self):
+    def index_(self):
         return self[1]
 
 

commit b0c2280f39803b370fba835a6e502c831a183e22

Fix confusion in `Person`'s dfs traversal methods.
- The issue was introduced in 678bb2a9de06e096318315e51ed437cf536ec3cb.
- No functional changes, just a semantic fix.

genealogy/family_tree.py
@@ -142,7 +142,7 @@ class FamilyTree:
         while True:
             for node in self.people:
                 if node not in visited:
-                    node.traverse_children_depth_first(visited, append)
+                    node.traverse_parents_depth_first(visited, append)
                     append(node)
             else:
                 break

genealogy/person.py
@@ -104,27 +104,6 @@ class Person:
             < (other.last_name, other.maiden_name, other.first_name, other.middle_name)
         )
 
-    def traverse_children_depth_first(
-            self,
-            visited: list[Person],
-            func: Callable[[Person], None],
-    ) -> bool:
-        """Traverse the family tree depth-first through children.
-
-        :param visited: List of already visited Person objects.
-        :param func: Function to apply to each Person.
-        :return: True if the traversal was successful, False if a cycle was detected.
-        """
-        if self in visited:
-            return False
-        visited.append(self)
-
-        for parent in self.parents.values():
-            rtn = parent.traverse_children_depth_first(visited, func)
-            if rtn:
-                func(parent)
-        return True
-
     def traverse_parents_depth_first(
             self,
             visited: list[Person],
@@ -134,14 +113,35 @@ class Person:
 
         :param visited: List of already visited Person objects.
         :param func: Function to apply to each Person.
-        :return: True if the traversal was successful, False if a cycle was detected.
+        :return: True if the Person was not visited, False otherwise.
+        """
+        if self in visited:
+            return False
+        visited.append(self)
+
+        for parent in self.parents.values():
+            rtn = parent.traverse_parents_depth_first(visited, func)
+            if rtn:
+                func(parent)
+        return True
+
+    def traverse_children_depth_first(
+            self,
+            visited: list[Person],
+            func: Callable[[Person], None],
+    ) -> bool:
+        """Traverse the family tree depth-first through children.
+
+        :param visited: List of already visited Person objects.
+        :param func: Function to apply to each Person.
+        :return: True if the Person was not visited, False otherwise.
         """
         if self in visited:
             return False
         visited.append(self)
 
         for child in self.children:
-            rtn = child.traverse_parents_depth_first(visited, func)
+            rtn = child.traverse_children_depth_first(visited, func)
             if rtn:
                 func(child)
         return True